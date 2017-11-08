73687d30590ba7a4af0b794e1af856e345907f07
juseong <bj2419@naver.com>
2017-11-09 02:57:01 +0900

    edit boot1.asm code

--- bootStrap/boot1.asm
+++ bootStrap/boot1.asm
@@ -69,6 +69,46 @@ PRINT_NEW_LINE:
 ; add your code here
 ; print current date to boch display
 
+		mov		si,0	;si레지스터에 0을 넣는다
+		mov 		bh,0	;bh에 0을 넣고
+		mov		ah,0x4	;현재 날짜를 알려주는 int 1a의 4h 함수를 
+		int		0x1a	;인터럽트를 발생시킨다
+		mov		bh,ch	;날짜중 연도의 상위두 글자(19h~20h)를 bh에 넣는다
+		call		PACK	;PACK으로 이동한다
+		mov		bh,cl	;날짜중 연도의 하위 두 글자 (00h~99h)를 bh에 넣는다
+		call		PACK
+		mov		bh,dh	;날짜중 월을 의미하는 (01h~12h)를 bh에 넣는다
+		call		PACK
+		mov		bh,dl	;날짜중 일을 의미하는 (01h~31h)를 bh에 넣는다
+		call		PACK
+		jmp		EPACK	;PACK
+PACK:
+		mov		bl,bh	;bh에 들어있는 1바이트값을 bl에 복사한다
+		shr		bh,4	;bh레지스터의 비트를 4비트 오른쪽으로 이동시킨다
+		add		bh,48	;이후 48 즉 16진수로 30h를 증가시키면 4비트에 대한 값이 8비트 10진수 아스키로 패킹이 된다
+		mov		[msg+si],bh;1바이트 아스키를 msg+si 주소위치에 저장한다
+		inc		si	;si레지스터를 1증가시킨다
+		and		bl,0x0f	;처음에 복사해둔 8비트 16진수 값의 상위 4비트를 0으로 만들고
+		add		bl,48	;10진수 아스키값으로 다시 패킹한다
+		mov		[msg+si],bl	;이값을 bl에서 msg+si 주소 위치에 저장한다
+		inc		si	;다시 si를 1증가시킨다
+		ret
+EPACK:	;날짜에대한 패킹이 끝나 날짜 데이터가 완성이 되면 
+
+
+		mov 		ax, 1301h	;인터럽트 10h의 13함수를 부를 준비를 한다
+		mov 		bx, 0x0e	;색상 값을 넣어준다 
+		mov 		cx, msgend- msg	;cx에는 msg와 msgend의 주소의 차를 이용하여 길이를 저장한다
+		mov 		dx, 40 ;출력될 위치는 80중에 가운데 40부터 시작한다
+		mov 		si, 0	;si레지스터를 초기화한뒤
+		push 		si	;스택에 넣어준다
+		pop 		es	;pop해주면서 넣어준 si의 값을 복구한다
+		mov 		bp, msg ;bp에 날자 데이터가 저장된 주소를 넣어준다 
+		int 		10h	;이후 인터럽트 10을 호출한다
+
+
+
+
 Activate_A20Gate:
 		mov		ax,	0x2401
 		int		0x15
@@ -260,6 +300,6 @@ ISR_IGNORE:
 
 msgRMode db "Real Mode", 0
 msgPMode db "Protected Mode", 0
-
- 
+msg db "99999999",0
+msgend:
 times 	2048-($-$$) db 0x00
